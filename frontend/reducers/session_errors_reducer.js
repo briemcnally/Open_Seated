58315a3f6e1a9a79c075521c7c595b9e98f91db7
brie_mcnally <brienne.mcnally@gmail.com>
2018-04-05 16:51:53 -0700

    Update CSS, Seeding

--- frontend/reducers/session_errors_reducer.js
+++ frontend/reducers/session_errors_reducer.js
@@ -1,10 +1,10 @@
-import { RECEIVE_SESSION_ERRORS,
+import { RECEIVE_ERRORS,
         RECEIVE_CURRENT_USER } from '../actions/session_actions';
 
 export default (state = [], action) => {
   Object.freeze(state);
   switch(action.type){
-    case RECEIVE_SESSION_ERRORS:
+    case RECEIVE_ERRORS:
       return action.errors;
     case RECEIVE_CURRENT_USER:
       return [];

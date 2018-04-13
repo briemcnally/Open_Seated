94bef05cc441e3487cb8f8a428497976873b31b6
brie_mcnally <brienne.mcnally@gmail.com>
2018-04-13 14:30:54 -0700

    Fix Ratings

--- frontend/components/restaurants/review_show.jsx
+++ frontend/components/restaurants/review_show.jsx
@@ -21,28 +21,33 @@ class ReviewShow extends React.Component {
   render() {
     if (this.props.reviews === undefined) return "Loading...";
     const restaurantId = this.props.match.params.restaurantId;
+    const serviceRating = this.props.restaurant.averageServiceRating.slice(0, 3);
+    const averageRating = this.props.restaurant.averageRating.slice(0, 3);
+    const averageFoodRating = this.props.restaurant.averageFoodRating.slice(0, 3);
+    const averageAmbienceRating = this.props.restaurant.averageAmbienceRating.slice(0, 3);
+    const averageValueRating = this.props.restaurant.averageValueRating.slice(0, 3);
     return (
       <div className="review-sub-box">
         <div className="review-header">
           <h1>What People Are Saying</h1>
           <h2>Overall ratings and reviews</h2>
           <p>Reviews can only be made by diners who have eaten at this restaurant</p>
-          <div className="overall-rating"><div className="rating-num">{this.props.restaurant.averageRating}</div> based on recent ratings</div>
+          <div className="overall-rating"><div className="rating-num">{averageRating}</div> based on recent ratings</div>
           <div className="review-specifics">
             <div className="rating">
-              <div className="rating-num">{this.props.restaurant.averageFoodRating}</div>
+              <div className="rating-num">{averageFoodRating}</div>
               <div>Food</div>
             </div>
             <div className="center-rating">
-              <div className="rating-num">{this.props.restaurant.averageServiceRating}</div>
+              <div className="rating-num">{serviceRating}</div>
               <div>Service</div>
             </div>
             <div className="center-rating">
-              <div className="rating-num">{this.props.restaurant.averageAmbienceRating}</div>
+              <div className="rating-num">{averageAmbienceRating}</div>
               <div>Ambience</div>
             </div>
             <div className="end-rating">
-              <div className="rating-num">{this.props.restaurant.averageValueRating}</div>
+              <div className="rating-num">{averageValueRating}</div>
               <div>Value</div>
             </div>
           </div>
